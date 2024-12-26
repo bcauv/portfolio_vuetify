72ced203182b9e51606488ac8abc333d02998da3
baptistecauvin <36502278+baptistecauvin@users.noreply.github.com>
2024-12-26 01:19:17 +0100

    feat: temporary description in about me

--- src/components/core/AppBar.vue
+++ src/components/core/AppBar.vue
@@ -1,8 +1,7 @@
 <template>
     <v-container>
         <v-app-bar :elevation="2" color="structure">
-            <v-app-bar-title><v-img :width="75" aspect-ratio="1/1"
-                    src="../../../public/logo_no_bg.png"></v-img></v-app-bar-title>
+            <v-app-bar-title><v-img :width="75" aspect-ratio="1/1" src="/logo_no_bg.png"></v-img></v-app-bar-title>
             <v-spacer />
             <select v-model="$i18n.locale" @change="handleLanguageChange($i18n.locale)" :class="[
                 'custom-select rounded pa-2 pe-8',
