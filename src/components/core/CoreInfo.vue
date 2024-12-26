54120d45607f09ed6c2bbda307f7a15e06583b2b
baptistecauvin <36502278+baptistecauvin@users.noreply.github.com>
2024-12-26 14:15:05 +0100

    chore: fix responsive for mobile

--- src/components/core/CoreInfo.vue
+++ src/components/core/CoreInfo.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container class="mt-0 pt-0">
     <v-row>
-      <v-col cols="2">
+      <v-col cols="12" md="2">
         <Summary />
       </v-col>
-      <v-col cols="10">
+      <v-col cols="12" md="10">
         <MainInfo />
       </v-col>
     </v-row>
