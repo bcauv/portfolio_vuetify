a3d3da19f3353e7d764b2c324590c95b67187eac
bcauv <bcauvin34@gmail.com>
2024-12-30 17:21:30 +0100

    chore: add info about bnp job

--- src/components/MainInfo/tabs/Resume.vue
+++ src/components/MainInfo/tabs/Resume.vue
@@ -7,8 +7,8 @@
     </v-expansion-panel>
     <v-expansion-panel :title="`${$t('resume.academic')}/${$t('resume.skills')}`">
       <v-expansion-panel-text>
-        <p>ECE Paris</p>
-        <p>English, Portuguese, Spanish</p>
+        <p>Engineer school ECE Paris 2016-2021</p>
+        <p>French (Native), English, Portuguese, Spanish</p>
       </v-expansion-panel-text>
     </v-expansion-panel>
     <v-expansion-panel :title="$t('resume.projects')">
