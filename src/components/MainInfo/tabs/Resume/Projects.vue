cb896e50132022541dd7b0cd225f0b6861f516b6
bcauv <bcauvin34@gmail.com>
2025-01-02 17:01:38 +0100

    chore: add more info to projects and add i18n

--- src/components/MainInfo/tabs/Resume/Projects.vue
+++ src/components/MainInfo/tabs/Resume/Projects.vue
@@ -1,30 +1,25 @@
 <template>
   <v-expansion-panels variant="accordion" color="primary">
-    <v-expansion-panel title="2020 PimpMyApp (Deloitte)">
+    <v-expansion-panel :title="$t('resume.projects.pimpMyApp.title')">
       <v-expansion-panel-text>
-        <p>
-          Prototyper, en 2 semaines, une web-app pour GRTgaz,
-          pour améliorer les conditions de télétravail pendant le confinement.
-          Utilisation de la méthode du Design Sprint et de différents outils, comme Mural et Figma
+        <p v-for="d in $tm('resume.projects.pimpMyApp.description')">
+          {{ d }}
         </p>
       </v-expansion-panel-text>
     </v-expansion-panel>
-    <v-expansion-panel title="2019-2020 FeetBack">
+    <v-expansion-panel :title="$t('resume.projects.feetBack.title')">
       <v-expansion-panel-text>
-        <h3>School Projet</h3>
-        <p>
-          Projet en équipe axé sur la santé.
-          Dispositif permettant un suivi a distance de l'usure et de la fiabilité des semelles orthopédique.
-          Composé d'un prototype, d'une application mobile et d'une application
+        <h3>{{ $t('resume.projects.feetBack.schoolProject') }}</h3>
+        <p v-for="d in $tm('resume.projects.feetBack.description')">
+          {{ d }}
         </p>
       </v-expansion-panel-text>
     </v-expansion-panel>
-    <v-expansion-panel title="2017-2018 Arm Of Hearing">
+    <v-expansion-panel :title="$t('resume.projects.armOfHearing.title')">
       <v-expansion-panel-text>
-        <h3>School Projet</h3>
-        <p>
-          Projet en équipe axé aussi sur la santé,
-          dispositif permettant de traduire le langage des signes en langage parlé.
+        <h3>{{ $t('resume.projects.armOfHearing.schoolProject') }}</h3>
+        <p v-for="d in $tm('resume.projects.armOfHearing.description')">
+          {{ d }}
         </p>
       </v-expansion-panel-text>
     </v-expansion-panel>
