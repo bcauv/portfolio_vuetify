4423c84935a87a470bac86786635af7a523dffb7
bcauv <bcauvin34@gmail.com>
2025-01-02 11:06:46 +0100

    chore: update tech_list to an array with a for loop to display

--- src/components/MainInfo/tabs/SiteInfo.vue
+++ src/components/MainInfo/tabs/SiteInfo.vue
@@ -2,11 +2,6 @@
   <h2>{{ $t('site_info.technical_info') }}</h2>
   <p>{{ $t('site_info.intro') }}</p>
   <ul class="default-list">
-    <li>{{ $t('site_info.tech_list.vuetify') }}</li>
-    <li>{{ $t('site_info.tech_list.vite') }}</li>
-    <li>{{ $t('site_info.tech_list.netlify') }}</li>
-    <li>{{ $t('site_info.tech_list.i18n') }}</li>
-    <li>{{ $t('site_info.tech_list.cookies') }}</li>
-    <li>{{ $t('site_info.tech_list.icons') }}</li>
+    <li v-for="tech in $tm('site_info.tech_list')"> {{ tech }}</li>
   </ul>
 </template>
